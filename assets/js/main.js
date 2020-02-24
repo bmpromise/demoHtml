e930de2b2dadbf8e7a9e4b5a6f3667862b5c534a
BM <bmpromise@gmail.com>
2020-02-24 18:53:58 +0800

    demo

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+(function(){
+  $(function(){
+    var $window = $(window),$body = $('body');
+    $body.addClass('is-loading');
+    $window.on('load', function() {
+      window.setTimeout(function() {
+        $body.removeClass('is-loading');
+      }, 100);
+    });
+    $("a").on('click',function(e){
+      console.log(this.href);
+      var txt = this.href,
+          lastF = txt.lastIndexOf("#");
+      if(lastF === -1){
+        return;
+      }
+      txt = txt.slice(lastF);
+      txt = txt.length===1 ? "#banner":txt;
+      console.log($(txt).position().top)
+      $('html,body').animate({scrollTop: $(txt).position().top},800);
+      e.preventDefault();
+    })
+    $(this).on('scroll',function(){
+      if($(this).scrollTop() >= $("#main").position().top){
+        $('header').removeClass('alt');
+      }else{
+        $('header').addClass('alt');
+      }
+    })
+  })
+})()
